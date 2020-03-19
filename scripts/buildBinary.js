6f6bee59e4e780b092195270f76908913bc64db2
brashtron <ashutosh0985@gmail.com>
2020-03-19 19:30:20 +0530

    windows compatibility issues

--- scripts/buildBinary.js
+++ scripts/buildBinary.js
@@ -21,8 +21,9 @@ shelljs.exec('pkg ./src/index.js -t host --out-path bin', (code, stderr, stdout)
         console.log('Error while packaging parking_lot');
         return 0;
     }
-	fs.rename('./bin/index*', fileName, function (err) {
+	fs.rename('./bin/index', fileName, function (err) {
 		if (err) {
+			console.log(err);
 			console.log('Error occurred while renaming the executable');
 		} else {
 			console.log('Parking Lot is packaged successfully for ' + os + 'host in "bin/" folder\n');
